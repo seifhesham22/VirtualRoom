1c83f9143b6eeabbfab1789e5f30bf13614c473c
Seif Elmoazen <seifhesham882@gmail.com>
2026-04-13 18:18:21 +0700

    feat/people-Module/Get-UnAssigned-Maintainers

---
+++ Modules.People.Application/Features/GetUnAssignedMaintainers/GetUnassignedMaintainersCommand.cs
@@ -0,0 +1,30 @@
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Modules.People.Application.Features.GetMaintainersByDepartment;
+using Modules.People.Application.Interfaces;
+using Shared.Authorization;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Modules.People.Application.Features.GetUnAssignedMaintainers
+{
+    public sealed record GetUnAssignedMaintainersCommand : IRequest<List<MaintainerSummary>>;
+
+    public sealed class GetUnAssignedMaintainersCommandHandler(IPeopleDbContext _people)
+        : IRequestHandler<GetUnAssignedMaintainersCommand, List<MaintainerSummary>>
+    {
+        public async Task<List<MaintainerSummary>> Handle(GetUnAssignedMaintainersCommand request, CancellationToken cancellationToken)
+        {
+            var maintainers = await _people
+                .maintainers
+                .Where(x => x.isAssigned == false)
+                .ToListAsync(cancellationToken);
+
+            return maintainers.Select(m => new MaintainerSummary(
+                m.Id,
+                m.FullName
+                )).ToList();
+        }
+    }
+}
